222fe6af20f8b540234498d13a6b2cde58ae3ae8
ajaytomar5421 <ajaytomar80060@gmail.com>
2024-06-04 18:42:04 +0530

    as name refers

---
+++ DATA_TYPES.java
@@ -0,0 +1,22 @@
+public class DATA_TYPES {
+    public static void main(String[] args){
+        byte data = 45;
+        short data1 = 1232;
+        int data2 = 43223;
+        long data3 = 4354243L;
+        // byte result =data+data1; error int is default and operation always count as in integer 
+        System.out.println(data);
+        System.out.println(data1);
+        System.out.println(data2);
+        System.out.println(data3);
+        // FLOAT data type -->
+        float num1 = 45.66f;
+        double num2 = 45.66;
+        System.out.println(num1);
+        char a ='a';
+        System.out.println(a);
+        // System.out.println("hello %d ", a); wrong statement
+        boolean state = true;
+        System.out.println(state);
+    }
+}
--- HELLO.java
+++ HELLO.java
@@ -1,5 +1,10 @@
 public class HELLO{
+    // static public void main(string[] args)   valid
+    // public static void main(string... args)  valid
+    // static public void main(string args[])   valid
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
     }
 }
